ff541b60ddce82ae542d96c724c9a7a9a6987452
Daren Bartolucci <darentbartolucci@gmail.com>
2021-04-10 22:09:32 -0700

    Spaceships and explosions added

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -17,15 +17,18 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         } 
         if(Phaser.Input.Keyboard.JustDown(keyF)){
-            this.isFiring = True;
+            this.isFiring = true;
         }
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
             this.y -= this.moveSpeed;
         }
         if(this.y <= borderUISize * 3 + borderPadding){
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
+    }
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
   
