55c170ab2d59342085e025fd0927798b452b503e
Daren Bartolucci <darentbartolucci@gmail.com>
2021-04-17 15:25:48 -0700

    New enemy

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -29,6 +29,9 @@ class Play extends Phaser.Scene{
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'New_sprite_2', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'New_sprite_2', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'New_sprite_2', 0, 10).setOrigin(0,0);
+        this.game.settings.spaceshipSpeed *=2;
+        this.ship04 = new Spaceship(this, game.config.width - 60, borderUISize*6 + borderPadding*4 +70, 'New_sprite_2', 0, 50).setOrigin(0,0);
+        
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -79,7 +82,14 @@ class Play extends Phaser.Scene{
         this.ship01.update();               // update spaceships (x3)
         this.ship02.update();
         this.ship03.update();
+        this.ship04.update();
         // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship04)) {
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship04);
+            
+
+        }
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
             this.shipExplode(this.ship03);
@@ -102,6 +112,7 @@ class Play extends Phaser.Scene{
             this.ship01.update();           // update spaceships (x3)
             this.ship02.update();
             this.ship03.update();
+            this.ship04.update();
         } 
         
     }
