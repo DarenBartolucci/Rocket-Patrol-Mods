8a19d3c9997893a6801eb701f9083fc3a0ba8dc5
Daren Bartolucci <darentbartolucci@gmail.com>
2021-04-17 19:37:00 -0700

    SFX changed

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -33,7 +33,7 @@ class Play extends Phaser.Scene{
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'New_sprite_2', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'New_sprite_2', 0, 10).setOrigin(0,0);
         this.game.settings.spaceshipSpeed *=2;
-        this.ship04 = new Spaceship(this, game.config.width - 60, borderUISize*6 + borderPadding*4 +70, 'Fast_enemy', 0, 50).setOrigin(0,0);
+        this.ship04 = new Spaceship(this, game.config.width - 60, borderUISize*6 + borderPadding*4 + 50, 'Fast_enemy', 0, 50).setOrigin(0,0);
         
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
