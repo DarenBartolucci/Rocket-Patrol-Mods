662a272e47d66c3241039db4c6de6d3c9edb1e02
Daren Bartolucci <darentbartolucci@gmail.com>
2021-04-17 20:45:32 -0700

    White borders removed for aesthetic purposes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -22,10 +22,10 @@ class Play extends Phaser.Scene{
         // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'New_sprite_5').setOrigin(0, 0);
         // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        //this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        //this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        //this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        //this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'New_sprite_1').setOrigin(0.5, 0);
         // add spaceships (x3)
