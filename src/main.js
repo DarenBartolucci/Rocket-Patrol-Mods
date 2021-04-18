20f6f4f90e004b027a5f1c45117d13ed77f16c10
Daren Bartolucci <darentbartolucci@gmail.com>
2021-04-17 23:05:33 -0700

    Final commit with score breakdown

--- src/main.js
+++ src/main.js
@@ -13,3 +13,16 @@ let borderPadding = borderUISize/3;
 
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
+//By Daren Bartolucci
+//Rocket Patrol Mod: REVENGE OF BATTLEZONE
+//Completion: ~11 hours
+//New, fourth enemy that is faster and worth more points (20)
+//Adds time bonus for enemies hit (20)
+//Complete overhall of art, sfx, and menus to create non-scifi theme (60)
+//This mod rethemes the game after the Atari arcade game Battlezone
+//Original spritework and neon-green theme is to recreate the graphics of original game
+//If it is found that not enough has been done for the 60 point modification, then;
+//New sprite work for everything (20)
+//Change in art and behavior for weapon (faster) (20)
+//Changed titlescreen with new art (10)
+
